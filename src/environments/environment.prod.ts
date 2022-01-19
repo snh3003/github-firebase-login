2840f526d2271b5afdcd46796cd34b83d548f66d
snh3003 <shahsama542@gmail.com>
2022-01-19 20:13:17 +0530

    dependencies with firebase auth and hosting added!!!

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
+  firebase: {
+    projectId: 'lco-proj',
+    appId: '1:356478994122:web:cd6aae0ff6cecd381d1633',
+    storageBucket: 'lco-proj.appspot.com',
+    apiKey: 'AIzaSyD2H2rE42NBjsYlEhxgdYU5GwrwQvaIG70',
+    authDomain: 'lco-proj.firebaseapp.com',
+    messagingSenderId: '356478994122',
+    measurementId: 'G-CH0ECTHLDF',
+  },
   production: true
 };
