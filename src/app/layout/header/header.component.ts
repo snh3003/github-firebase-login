c6fc02f8130848d621dd66c00119e5c7754e7e3e
snh3003 <shahsama542@gmail.com>
2022-01-19 23:02:03 +0530

    auth service signout done!!!

--- src/app/layout/header/header.component.ts
+++ src/app/layout/header/header.component.ts
@@ -1,15 +1,43 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from 'src/app/services/auth.service';
+import { Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.css']
+  styleUrls: ['./header.component.css'],
 })
 export class HeaderComponent implements OnInit {
 
-  constructor() { }
+  email: any = null;
+  password: string = '';
 
-  ngOnInit(): void {
+  constructor(
+    private authService: AuthService,
+    private router: Router,
+    private toastr: ToastrService
+  ) {
+    authService.getUser().subscribe((user) => {
+      this.email = user!.email;
+    });
   }
 
+  userSignIn = (email: string, password: string) => {
+    this.authService.signIn(email, password);
+  };
+
+  async userSignOut() {
+    try {
+      const res = await this.authService.signOut();
+      this.router.navigateByUrl(['/signin']);
+      this.toastr.success('You have been signed out');
+      this.toastr.info('Login again to continue');
+      this.email = null;
+    }catch(e: any){
+      this.toastr.error(e.message);
+    }
+  }
+
+  ngOnInit(): void {}
 }
