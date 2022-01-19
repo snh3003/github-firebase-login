9f0afe6d930eaa7bf72e72fe0852c4dc49535a44
snh3003 <shahsama542@gmail.com>
2022-01-19 22:37:24 +0530

    github service auth done

--- src/app/services/github-auth.service.ts
+++ src/app/services/github-auth.service.ts
@@ -1,9 +1,15 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class GithubAuthService {
+  private gitUri = 'https://api.github.com/users/';
+  constructor(private httpClient: HttpClient) {}
 
-  constructor() { }
+  getUser = (username: string) => this.httpClient.get(this.gitUri + username);
+
+  getRepos = (username: string) =>
+    this.httpClient.get(`this.gitUri${username}/repos`);
 }
