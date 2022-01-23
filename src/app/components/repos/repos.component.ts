0b2bef7222cb96d6ed148618d65080f7d931b35b
snh3003 <shahsama542@gmail.com>
2022-01-23 22:58:40 +0530

    html component structures completed!!!

--- src/app/components/repos/repos.component.ts
+++ src/app/components/repos/repos.component.ts
@@ -1,15 +1,41 @@
-import { Component, OnInit } from '@angular/core';
+import {
+  Component,
+  OnInit,
+  Input,
+  OnChanges,
+  ChangeDetectorRef,
+} from '@angular/core';
+import { GithubAuthService } from 'src/app/services/github-auth.service';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-repos',
   templateUrl: './repos.component.html',
-  styleUrls: ['./repos.component.css']
+  styleUrls: ['./repos.component.css'],
 })
 export class ReposComponent implements OnInit {
+  @Input() repoUri: string;
+  repos: any;
 
-  constructor() { }
+  constructor(
+    private gitHubService: GithubAuthService,
+    private ref: ChangeDetectorRef,
+    private toastr: ToastrService
+  ) {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  ngOnChanges(): void {
+    if (this.repoUri) {
+      this.gitHubService.getRepos(this.repoUri).subscribe(
+        (repos: any) => {
+          this.repos = repos;
+          this.ref.detectChanges();
+        },
+        (error) => {
+          this.toastr.error(error);
+        }
+      );
+    }
   }
-
 }
